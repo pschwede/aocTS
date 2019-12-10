beea72257e829f7acfb96d8ab5ce20f08236d69c
Peter Schwede <pschwede@posteo.de>
2019-12-10 17:34:47 +0100

    chg: slightly better readability

--- 03b.ts
+++ 03b.ts
@@ -123,14 +123,16 @@ U7,R6,D4,L4`))
 U62,R66,U55,R34,D71,R55,D58,R83`))
         && assertEquals(135, shortestDistanceFromOriginOfOverlay(`R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
 U98,R91,D20,R16,D67,R40,U7,R15,U6,R7`))
-        && assertEquals(610, shortestDistanceFromOriginOfOverlay(`R75,D30,R83,U83,L12,D49,R71,U7,L72
-U62,R66,U55,R34,D71,R55,D58,R83`,
-            (a: [number, number]) => `R75,D30,R83,U83,L12,D49,R71,U7,L72
-U62,R66,U55,R34,D71,R55,D58,R83`.split("\n").map(
-    (b) => WireMap.fromString(b, "" + a[0] + "x" + a[1]).steps).reduce((c, d) => c + d, 0)))
-        && assertEquals(410, shortestDistanceFromOriginOfOverlay(`R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
-U98,R91,D20,R16,D67,R40,U7,R15,U6,R7`,(a: [number, number]) => `R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
-U98,R91,D20,R16,D67,R40,U7,R15,U6,R7`.split("\n").map((b)=>WireMap.fromString(b, "" + a[0] + "x" + a[1]).steps).reduce((c,d)=>c+d,0)));
+        && ((x) => assertEquals(610, shortestDistanceFromOriginOfOverlay(x,
+            (a: [number, number]) => x.split("\n").map(
+                (b) => WireMap.fromString(b, "" + a[0] + "x" + a[1]).steps).reduce(
+                    (c, d) => c + d, 0))))(`R75,D30,R83,U83,L12,D49,R71,U7,L72
+U62,R66,U55,R34,D71,R55,D58,R83`)
+        && ((x) => assertEquals(410, shortestDistanceFromOriginOfOverlay(x,
+            (a: [number, number]) => x.split("\n").map(
+                (b) => WireMap.fromString(b, "" + a[0] + "x" + a[1]).steps).reduce(
+                    (c, d) => c + d, 0))))(`R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+U98,R91,D20,R16,D67,R40,U7,R15,U6,R7`);
 }
 
 function main(): number {
